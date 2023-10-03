22d1b55c1d8bec493248ba17eb6b201b4fdfaa7d
Manjesh R V <manjeshrv592@gmail.com>
2023-10-03 15:32:57 +0530

    Added color & font variables along with util classes for text color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Recipe Crush</h1>;
+  return (
+    <>
+      <h1 className="text-gray-900">Recipe Crush</h1>
+      <p>Search your favorite recipes</p>
+    </>
+  );
 };
 
 export default App;
